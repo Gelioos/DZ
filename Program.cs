da863f913d1e3dc5a2b4f466e69f5d50660352f5
Danil <martykhin84@gmail.com>
2023-02-12 17:57:25 +0300

    DZ

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿// Console.WriteLine("Input number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine(num1);
+// }
+// else
+// {
+// Console.WriteLine(num2);
+// }
+
+
+
+// Console.WriteLine("Input number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine("Input number");
+//  int num2 = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine("Input number");
+//  int num3 = Convert.ToInt32(Console.ReadLine());
+
+//  int max = num1;
+//  if (max <= num1) max = num1;
+//  if (max <= num2) max = num2;
+//  if (max <= num3) max = num3;
+//  Console.WriteLine(max);
+
+// 
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num % 2 == 1)
+// {
+//     Console.WriteLine("Число "+num+" является НЕЧЕТНЫМ");
+// }
+// else
+// {
+//     Console.WriteLine("Число "+num+" является ЧЕТНЫМ");
+// }
+ 
+
+ 
+ 
+
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int current = 1;
+// if (num >= 1)
+// {
+//     while (current <= num)
+//     {
+//       if (current % 2 == 1)
+//       {
+//        current++;
+//       }
+//       else
+//       {
+//         Console.WriteLine(current + " ");
+//     current++;
+//       }
+//     }
+        
+// }
+// else
+// {
+//     Console.WriteLine("Нет четных");
+// }
+        
+    
+    
